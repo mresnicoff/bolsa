b90da177f2a5857d07f6e0b7b338bd90b17be211
Dipen Khadka <dipenkhadka191@gmail.com>
2023-10-13 20:20:25 +0545

    Manual Dark & Light Mode Switch Added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { navLinks } from "../../Constants";
+import { MdLightMode } from "react-icons/md";
 
 const Header = () => {
+  const toggleTheme = () =>{
+    document.documentElement.classList.toggle("dark")
+  }
+
   return (
     <header>
       <nav className=" w-full Header flex justify-between items-center pt-8 pb-8">
@@ -15,7 +20,7 @@ const Header = () => {
             </h1>
           </a>
         </div>
-        <ul className="hidden sm:flex items-center gap-3 relative ">
+        <ul className="hidden sm:flex items-center gap-2">
           {navLinks.map((li) => (
             <li
               className="text-[#6f6f6f] dark:text-slate-100 text-base hover:text-[#2a68ff]"
@@ -24,13 +29,18 @@ const Header = () => {
               <a href={li.href}>{li.label}</a>
             </li>
           ))}
+          <MdLightMode onClick={toggleTheme} className="dark:invert cursor-pointer ml-4 h-5 w-full" />
         </ul>
-        <div className="flex leading-4 flex-col gap-1 sm:hidden ">
+        <div className="absolute right-20">
+        <MdLightMode onClick={toggleTheme} className="dark:invert  h-5 w-full sm:hidden"/>
+        </div>
+        <div className="flex leading-4 flex-col gap-1 sm:hidden">
           <div className="h-[3px] dark:bg-slate-100 bg-black w-8"></div>
           <div className="h-[3px] dark:bg-slate-100 bg-black w-8"></div>
           <div className="h-[3px] dark:bg-slate-100 bg-black w-8"></div>
         </div>
       </nav>
+
     </header>
   );
 };
